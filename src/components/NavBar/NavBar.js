be4b861bdeef96e616c3e428101cfc2a2b7d40c8
spfox24 <68411909+spfox24@users.noreply.github.com>
2021-01-06 17:59:27 -0600

    adds nav styling

--- src/components/NavBar/NavBar.js
+++ src/components/NavBar/NavBar.js
@@ -4,21 +4,21 @@ import logo from '../../img/gvlogo.png';
 
 const NavBar = (props) => {
     return (
-        <div className="NavBar">
-            <div className="Logo-link">
-                <Link to='/'>
-                <img src={logo} className="Logo"/>
-                </Link>
-            </div>
-                <ul className="links">
-                    <li className="nav-link">
-                        <Link to='/login' className="NavBar-link">Login</Link>
-                    </li>
-                    <li className="nav-link">
-                        <Link to='/signup' className="NavBar-link">Signup</Link>
-                    </li> 
-                </ul>   
-        </div>
+        <nav className="NavBar">
+            <ul className="links">
+                <li className="Logo-link">
+                    <Link to='/'>
+                        <img src={logo} className="Logo"/>
+                    </Link>
+                </li>
+                <li className="nav-link">
+                    <Link to='/login' className="NavBar-link">Login</Link>
+                </li>
+                <li className="nav-link">
+                    <Link to='/signup' className="NavBar-link">Signup</Link>
+                </li>  
+            </ul>
+        </nav>
     );
 };
 
